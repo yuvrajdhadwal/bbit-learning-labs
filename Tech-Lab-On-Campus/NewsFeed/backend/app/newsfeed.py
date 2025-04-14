b3bb4a882ea9b396a1336e7eb325b8a8e02758ff
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2025-04-14 18:06:49 -0400

    started on bloomberg project

--- Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
+++ Tech-Lab-On-Campus/NewsFeed/backend/app/newsfeed.py
@@ -2,6 +2,7 @@
 
 from dataclasses import dataclass
 from datetime import datetime
+from app.utils.redis import REDIS_CLIENT
 
 
 @dataclass
@@ -21,11 +22,16 @@ def get_all_news() -> list[Article]:
     # 1. Use Redis client to fetch all articles
     # 2. Format the data into articles
     # 3. Return a list of the articles formatted 
+    print(REDIS_CLIENT.get_entry("all_articles"))
     return []
 
 
 def get_featured_news() -> Article | None:
     """Get the featured news article from the datastore."""
     # 1. Get all the articles
-    # 2. Return as a list of articles sorted by most recent date
+    # 2. Return most recently published article
     return None
+
+def __create_article_object(author: str, title: str, body: str, publish_date: datetime,
+                            image_url: str, url: str) -> None:
+    return Article(author, title, body, publish_date, image_url, url)
